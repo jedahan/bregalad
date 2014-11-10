14da2428e431cca60889f7d7a225e4bfa572c9c8
Jonathan Dahan <jonathan@jedahan.com>
2014-11-10 12:34:51 -0500

    quote better, no sftp

--- backup.sh
+++ backup.sh
@@ -1,24 +1,14 @@
-[ -e ./config.sh ] && . ./config.sh
-
 pushd backup
 
 TODAY=$(date +%y%M%d)
-NOW=$(date +%y%M%d%H%m%S)
+NOW=$(date +%y-%M-%d-%H:%m:%S)
 TODAY_START=$(date --date=$(date +%Y-%m-%d) +%s)
 TODAY_END=$(date --date="$(date +%Y-%m-%d) 1 day" +%s)
 
 echo $TODAY_START $TODAY_END
 
-curl localhost:5000/participants.csv?start=$TODAY_START&end=$TODAY_END > ${NOW}.csv
+curl "localhost:5000/participants.csv?start=${TODAY_START}&end=${TODAY_END}" > ${NOW}.csv
 curl localhost:5000/participants.csv > participants.csv
-cp participants.db ${NOW}.db
-
-sftp -i ${IDENTITY:-${HOME}/.ssh/id_rsa} ${USERNAME:-$USER}@${HOST:-127.0.0.1} << EOF
-put participants.db
-put participants.csv
-put ${NOW}.db
-put ${NOW}.csv
-bye
-EOF
+cp ../participants.db ${NOW}.db
 
 popd
