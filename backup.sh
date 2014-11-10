343b624d96a05ffa77e2d7d7162c76f13596c481
Jonathan Dahan <jonathan@jedahan.com>
2014-11-10 13:03:23 -0500

    be smarter about dates

--- backup.sh
+++ backup.sh
@@ -1,13 +1,13 @@
 pushd backup
 
 TODAY=$(date +%y%M%d)
-NOW=$(date +%y-%M-%d-%H:%m:%S)
-TODAY_START=$(date --date=$(date +%Y-%m-%d) +%s)
-TODAY_END=$(date --date="$(date +%Y-%m-%d) 1 day" +%s)
+NOW=$(date +%Y-%m-%d_%H:%M:%S)
+TODAY_START=$(date --date="$(date +%Y-%m-%d)" +%s)
+TODAY_END=$(($TODAY_START + 86400))
 
 echo $TODAY_START $TODAY_END
 
-curl "localhost:5000/participants.csv?start=${TODAY_START}&end=${TODAY_END}" > ${NOW}.csv
+echo curl "localhost:5000/participants.csv?start=${TODAY_START}&end=${TODAY_END}" > ${NOW}.csv
 curl localhost:5000/participants.csv > participants.csv
 cp ../participants.db ${NOW}.db
 
