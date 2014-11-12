c6b88b99ae48bf195a017ff1303039c2909b0810
Jonathan Dahan <jonathan@jedahan.com>
2014-11-11 21:50:18 -0500

    backquote & in shell script

--- backup.sh
+++ backup.sh
@@ -3,8 +3,9 @@ NOW=$(date +%Y-%m-%d_%H:%M:%S)
 
 pushd backup
 TODAY_START=$(date --date="$(date +%Y-%m-%d)" +%s)
+TODAY_END=$(( $TODAY_START + 86400 ))
 
-curl --silent localhost:5000/participants.csv?start="${TODAY_START}" > ${NOW}.csv
+curl --silent localhost:5000/participants.csv?start=${TODAY_START}\&end=${TODAY_END} > ${NOW}.csv
 curl --silent localhost:5000/participants.csv > participants.csv
 cp ../participants.db ${NOW}.db
 
