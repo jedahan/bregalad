728899cf3e7d1398aeb0c1b2539b1fcc15e301ce
Jonathan Dahan <jonathan@jonathan.is>
2015-12-09 11:32:29 -0500

    Allow multiple emails for daily report

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-NOW=$(date +%Y-%m-%d_%H:%M:%S)
-source config.sh
-
 pushd backup
+NOW=$(date +%Y-%m-%d_%H:%M:%S)
 TODAY_START=$(gdate --date="$(date +%Y-%m-%d)" +%s%3N)
 TODAY_END=$(( $TODAY_START + 86400 *1000 ))
 
 curl --silent localhost:5000/participants.csv?start=${TODAY_START}\&end=${TODAY_END} > ${NOW}.csv
 curl --silent localhost:5000/participants.csv > participants.csv
 cp ../participants.db ${NOW}.db
-
-[[ ! -z $BREGALAD_REPORT_EMAIL ]] && \
-  curl -X POST -d "{\"email\":\"$BREGALAD_REPORT_EMAIL\"}" -H "Content-Type: application/json" localhost:5000/report
 popd
+
+source config.sh
+for email in "${BREGALAD_REPORT_EMAILS[@]}"; do
+  curl -X POST -d "{\"email\":\"$email\"}" -H "Content-Type: application/json" localhost:5000/report
+done
